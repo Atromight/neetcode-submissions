7569291a2d3354b5300e064fda9f26b872cfa686
atrom <56192970+Atromight@users.noreply.github.com>
2026-05-25 15:34:34 +0100

    Add: palindrome-partitioning - submission-1

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-1.py
@@ -0,0 +1,36 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        res = []
+        letters = list(s)
+        n = len(letters)
+
+        def is_palindrome(word: str) -> bool:
+            m = len(word)
+            for i in range(0, m//2):
+                if word[i] != word[m-i-1]:
+                    return False
+
+            return True
+
+
+        def backtrack(i: int, curr: List[str]):
+            prev = curr[-1]
+            if i == n:
+                if is_palindrome(prev):
+                    res.append(curr)
+
+                return
+
+            letter = letters[i]
+            if is_palindrome(prev):
+                backtrack(i+1, curr + [letter])
+
+            curr[-1] += letter            
+            backtrack(i+1, curr)
+
+            return
+
+
+        backtrack(1, [letters[0]])
+
+        return res
