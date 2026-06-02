ad5d0302af3751a1fc74ba290a0bf79a6c478169
atrom <56192970+Atromight@users.noreply.github.com>
2026-06-02 16:47:56 +0100

    Add: course-schedule - submission-12

---
+++ Data Structures & Algorithms/course-schedule/submission-12.py
@@ -0,0 +1,20 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        edges = [[] for _ in range(numCourses)]
+        for dest, src in prerequisites:
+            edges[src].append(dest)
+
+        for i in range(numCourses):
+            if edges[i]:
+                q = deque([])
+                q.append([i, set()])
+                while q:
+                    src, curr_v = q.popleft()
+                    for dest in edges[src]:
+                        if dest in curr_v:
+                            return False
+
+                        curr_v.add(src)
+                        q.append([dest, curr_v.copy()])
+
+        return True
