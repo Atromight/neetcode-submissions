602ac58cff56695b4803ba2858f6ca0d28012112
atrom <56192970+Atromight@users.noreply.github.com>
2026-06-02 20:19:08 +0100

    Add: valid-tree - submission-6

---
+++ Data Structures & Algorithms/valid-tree/submission-6.py
@@ -0,0 +1,40 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        vertix_map = [[] for _ in range(n)]
+        for src, dest in edges:
+            if src == dest:
+                return False
+
+            vertix_map[src].append(dest)
+            vertix_map[dest].append(src)
+        
+        if n == 1:
+            return True
+
+        for i in range(n):
+            visited = set()
+            if vertix_map[i]:
+                q = deque([])
+                q.append([i, None, set()])
+                while q:
+                    src, prev, curr_v = q.popleft()
+                    for dest in vertix_map[src]:
+                        if dest == prev:
+                            continue
+
+                        else:
+                            if dest in curr_v:
+                                return False
+
+                        curr_v.add(src)
+                        q.append([dest, src, curr_v.copy()])
+                    
+                    visited.add(src)
+
+                if len(visited) != n:
+                    return False
+
+            else:
+                return False
+
+        return True
