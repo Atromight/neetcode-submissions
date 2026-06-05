66341f917ba7c178cbf7576f61e8993c5bf5330e
atrom <56192970+Atromight@users.noreply.github.com>
2026-06-05 15:51:22 +0100

    Add: coin-change - submission-1

---
+++ Data Structures & Algorithms/coin-change/submission-1.py
@@ -0,0 +1,47 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        n = len(coins)
+        coins.sort()
+        self.res = amount + 1
+
+        def dfs(i: int, rem: int, m: int) -> None:
+            if i < 0:
+                return
+
+            val = coins[i]
+            div = rem // val
+            mod = rem % val
+            while div >= 0:
+                if mod == 0:
+                    cur = m + div
+                    if cur < self.res:
+                        self.res = cur
+                        return
+
+                else:
+                    dfs(i-1, mod, m + div)
+
+                div -= 1
+                mod += val
+
+            return
+        
+        dfs(n-1, amount, 0)
+
+        if self.res > amount:
+            return -1
+
+        return self.res
+        
+        
+
+
+
+
+
+
+
+
+
+
+
