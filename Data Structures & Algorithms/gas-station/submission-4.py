0382ddfa06e31d95c603815e21abd4311c5d4c88
atrom <56192970+Atromight@users.noreply.github.com>
2026-06-10 11:37:46 +0100

    Add: gas-station - submission-4

---
+++ Data Structures & Algorithms/gas-station/submission-4.py
@@ -0,0 +1,35 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        n = len(gas)
+        max_sum = gas[0] - cost[0]
+        cur_sum = 0
+        start_pos = 0
+        for i in range(n):
+            rem = gas[i] - cost[i] # remaining gas
+            print(f"i: {i}, rem: {rem}, cur_sum: {cur_sum}, max_sum: {max_sum}")
+            if cur_sum < 0:
+                start_pos = i
+                cur_sum = 0
+
+            cur_sum += rem
+            if cur_sum > max_sum:
+                max_sum = cur_sum
+                # start_pos = i
+            
+            # print(f"i: {i}, rem: {rem}, cur_sum: {cur_sum}, max_sum: {max_sum}")
+
+        j = 0
+        cur_sum = 0
+        while j <= n:
+            if cur_sum < 0:
+                return -1
+
+            i = start_pos + j
+            if i >= n:
+                i = i - n
+
+            rem = gas[i] - cost[i]
+            cur_sum += rem
+            j += 1
+
+        return start_pos
