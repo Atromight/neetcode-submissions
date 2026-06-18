9fe559355656d5e92e952c8c685b919449806943
atrom <56192970+Atromight@users.noreply.github.com>
2026-06-18 21:53:10 +0100

    Add: spiral-matrix - submission-1

---
+++ Data Structures & Algorithms/spiral-matrix/submission-1.py
@@ -0,0 +1,45 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        m = len(matrix)
+        n = len(matrix[0])
+        res = []
+        i, j = 0, -1
+        directions_map = {
+            "right":{
+                "steps": n,
+                "next": "down"
+            },
+            "down":{
+                "steps": m-1,
+                "next": "left"
+            },
+            "left":{
+                "steps": n-1,
+                "next": "up"
+            },
+            "up":{
+                "steps": m-2,
+                "next": "right"
+            }
+        }
+        direction = "right"
+        while True:
+            steps = directions_map[direction]["steps"]
+            while steps > 0:
+                if direction == "right":
+                    j += 1
+                elif direction == "down":
+                    i += 1
+                elif direction == "left":
+                    j -= 1
+                elif direction == "up":
+                    i -= 1
+
+                res.append(matrix[i][j])
+                steps -= 1
+
+            if len(res) == m * n:
+                return res
+
+            directions_map[direction]["steps"] -= 2
+            direction = directions_map[direction]["next"]
